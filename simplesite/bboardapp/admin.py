5853d699a78f87b47cb03d00521d10a14b8f62a7
pmtkachev <ptka4ev@mail.ru>
2023-03-24 22:58:31 +0300

    day_3

--- simplesite/bboardapp/admin.py
+++ simplesite/bboardapp/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Bb
+from .models import Rubric
 
 
 class AdminBb(admin.ModelAdmin):
-    list_display = ('title', 'text', 'price', 'published')
+    list_display = ('title', 'text', 'price', 'rubric', 'published')
     list_display_links = ('title', 'text')
     search_fields = ('title', 'text')
 
 
 admin.site.register(Bb, AdminBb)
+admin.site.register(Rubric)
--- simplesite/bboardapp/views.py
+++ simplesite/bboardapp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from .models import Bb
+from .models import Bb, Rubric
 
 
 def index(request):
     bbs = Bb.objects.all()
-    context = {'bbs': bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'bboardapp/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    cur_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'cur_rubric': cur_rubric}
+    return render(request, 'bboardapp/by_rubric.html', context)
