26c35796486298e28c80e65c1d35aaa5b6e7418b
pmtkachev <ptka4ev@mail.ru>
2023-03-25 21:40:15 +0300

    day 4

--- simplesite/bboardapp/apps.py
+++ simplesite/bboardapp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BboardappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bboardapp'
+    verbose_name = 'Объявления'
